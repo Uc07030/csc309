29b1d9ed385a30748c048cb78043e3339cd60999
Uc07030 <139179054+Uc07030@users.noreply.github.com>
2023-07-18 21:16:29 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,28 +1,77 @@
 <!DOCTYPE html>
+
+
+
 <html>
+
 <head>
+
     <title>CSC 309 DB Class</title>
+
 </head>
+
 <body>
+
     <h2>Registration Form</h2>
 
+
+
     <form action="process.php" method="post">
+
         <div>
+
             <label for="firstname">Firstname:</label>
+
             <input type="text" name="firstname">
+
         </div>
 
+
+
         <div>
+
             <label for="lastname">Lastname:</label>
+
             <input type="text" name="lastname">
+
+        </div>
+
+
+
+        <div>
+
+            <label for="email">Email:</label>
+
+            <input type="email" name="email">
+
         </div>
 
         <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email">
+
+            <label for="Gender">Gender:</label>
+
+            <input type="radio" name="gender" value="male" required>male
+
+            <input type="radio" name="gender" value="female" required>female
+
+            <input type="radio" name="gender" value="other" required>other
+
         </div>
 
-        <button type="submit">Submit</button>
+        <div>
+
+            <label for="date_of_birth">Date of birth:</label>
+
+            <input type="date" name="date_of_birth">
+
+        </div>
+
+
+
+        <button type="submit" name="submit">Submit</button>
+
     </form>
+
 </body>
+
 </html>
