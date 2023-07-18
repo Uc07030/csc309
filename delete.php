6706616120591821047f6dbe20bb59977e3493d3
Uc07030 <139179054+Uc07030@users.noreply.github.com>
2023-07-18 20:52:19 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,29 +1,48 @@
-
 <?php
+
     $server = 'localhost'; // 127.0.0.1
+
     $username = 'root';
-    $password = 'rootroot';
+
+    $password = '';
+
     $db = 'csc309';
 
+
+
     // Open a new connection
+
     $con = new mysqli($server, $username, $password, $db);
 
+
+
     // Check connection
+
     if ($con->connect_error){
+
         die("Connection failed: " . $con->connect_error);
+
     }
 
-    // Query
-    $sql = "DELETE FROM users WHERE id = 3";
+ //query  to delete user having id of 3  from the database 
 
-    $result = $con->query($sql);
+ $sql="DELETE FROM users WHERE id=3";
 
-    if ($result === true) {
-        echo 'Record deleted successfully';
-    } else {
-        echo 'Error deleting record: ' . $con->error;
-    }
+ $result=$con->query($sql);
 
-    // Close connection
-    $con->close();
-?>
+ if($result===true){
+     //checking to see if it is successfully deleted 
+
+    echo 'record deleted sucessfully';
+
+ }else{
+
+    echo 'Error deleting recor:'.$con->error;
+
+ }
+
+ //close connection
+
+ $con->close();
+
+ ?>
