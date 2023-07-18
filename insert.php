ed002f8768c3b95cfb21ee10a8ba5ce172190e8c
Uc07030 <139179054+Uc07030@users.noreply.github.com>
2023-07-18 21:04:03 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,30 +1,54 @@
-
 <?php
+
     $server = 'localhost'; // 127.0.0.1
+
     $username = 'root';
-    $password = 'rootroot';
-    $db = 'csc309';
+
+    $password = '';
+
+    $db = 'csc309';// creating database
+
+
 
     // Open a new connection
+
     $con = new mysqli($server, $username, $password, $db);
 
+
+
     // Check connection
+
     if ($con->connect_error){
+
         die("Connection failed: " . $con->connect_error);
+
     }
 
-    // Create a Table
+//inserting   'Nwafor', 'Benjamin', 'benjaminn@gmai to users table 
     $sql = "INSERT INTO users (firstname, lastname, email) 
+
     VALUES ('Nwafor', 'Benjamin', 'benjaminn@gmail.com')";
 
-    $result = $con->query($sql);
+
+
+    $result = $con->query($sql);//allowing access to the database
+
+
 
     if ($result === true) {
+
         echo 'Record inserted successfully';
+
     } else {
+
         echo 'Error inserting record: ' . $con->error;
+
     }
 
+
+
     // Close connection
+
     $con->close();
+
 ?>
