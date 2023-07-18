a9992b0d0ed2933a96718981526c847d75eb3709
Uc07030 <139179054+Uc07030@users.noreply.github.com>
2023-07-18 21:20:05 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,29 +1,59 @@
 
+
 <?php
+
+
+
     $server = 'localhost'; // 127.0.0.1
+
     $username = 'root';
-    $password = 'rootroot';
+
+    $password = '';
+
     $db = 'csc309';
 
+
+
     // Open a new connection
+
     $con = new mysqli($server, $username, $password, $db);
 
+
+
     // Check connection
+
     if ($con->connect_error){
+
         die("Connection failed: " . $con->connect_error);
+
     }
 
-    // Query
+
+
+    // Query to update user name to..
+
     $sql = "UPDATE users SET firstname = 'Iruoma' WHERE id = 3";
+
+
 
     $result = $con->query($sql);
 
+
+
     if ($result === true) {
+
         echo 'Record updated successfully';
+
     } else {
+
         echo 'Error updating record: ' . $con->error;
+
     }
 
+
+
     // Close connection
+
     $con->close();
+
 ?>
