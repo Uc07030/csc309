882039760cf92de4d8dcb798d2565382e42cc43b
Uc07030 <139179054+Uc07030@users.noreply.github.com>
2023-07-18 21:14:41 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,33 +1,85 @@
-<?php
-    $server = 'localhost'; // 127.0.0.1
+  $server = 'localhost'; // 127.0.0.1
+
     $username = 'root';
-    $password = 'rootroot';
+
+    $password = '';
+
     $db = 'csc309';
 
+
+
     // Open a new connection
+
     $con = new mysqli($server, $username, $password, $db);
 
+
+
     // Check connection
+
     if ($con->connect_error){
+
         die("Connection failed: " . $con->connect_error);
+
     }
 
-    // Query
+
+
+    // Query  once the submit button is cliked the data will be sent to the database
+// and to users table 
+    if(isset($_POST['submit'])){ 
+//WE  are storing the key value pairs from our formt to $variabls=$_POST ['name'];
     $firstname = $_POST['firstname'];
+
     $lastname = $_POST['lastname'];
+
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('$firstname', '$lastname', '$email')";
+    $gender = $_POST['gender'];
 
-    $result = $con->query($sql);
+    $date_of_birth = $_POST['date_of_birth'];
+
+
+
+    $sql = "INSERT INTO users (`firstname`, `lastname`, `email`,`gender`,`date_of_birth`) 
+
+    VALUES ('$firstname', '$lastname', '$email','$gender', '$date_of_birth ')";// variables containing values from form 
+
+ 
+
+     $result = $con->query($sql);
+
+
 
     if ($result === true) {
+// checking if records are successfully sent to the dabase table users
         echo 'Record inserted successfully';
+
     } else {
+
         echo 'Error inserting record: ' . $con->error;
+
     }
 
-    // Close connection
+
+
+
+
+    }
+
+    
+
+   
+
+
+
+    
+
     $con->close();
+
+
+
+    // Close connection
+
+
+
 ?>
